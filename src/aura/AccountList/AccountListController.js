c15716c9da8bf645d37d72f1430f792377631382
Miguel Núñez Díaz-Montes <m.nunez.diaz-montes@accenture.com>
2018-10-12 13:34:36 +0200

    Update AccountList (implemented in other module) in order to pass module

--- src/aura/AccountList/AccountListController.js
+++ src/aura/AccountList/AccountListController.js
@@ -9,6 +9,14 @@
                 event.fire();
             }), 500);
         });
-    $A.enqueueAction(action);
+        $A.enqueueAction(action);
+        helper.getAccountList(component);
+    },
+    deleteAccount: function(component, event, helper) {
+        // Prevent the form from getting submitted
+        event.preventDefault();
+        // Get the value from the field that's in the form
+        var accountName = event.target.getElementsByClassName('account-name')[0].value;
+        confirm('Delete the ' + accountName + ' account? (don’t worry, this won’t actually work!)');
     }
 })
