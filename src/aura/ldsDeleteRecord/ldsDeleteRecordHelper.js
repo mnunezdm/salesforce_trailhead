cbb098223605b6a282a5795026ec1330e831d5ea
Miguel Núñez Díaz-Montes <m.nunez.diaz-montes@accenture.com>
2018-10-13 12:26:28 +0200

    Implement show toast in helper (work on app display) ; Add slds alert ; Fix Layout

--- src/aura/ldsDeleteRecord/ldsDeleteRecordHelper.js
+++ src/aura/ldsDeleteRecord/ldsDeleteRecordHelper.js
@@ -1,5 +1,15 @@
 ({
-    helperMethod : function() {
-
+    showToast : function(title, message) {
+        var resultsToast = $A.get("e.force:showToast");
+        if (resultsToast) {
+            resultsToast.setParams({
+                "title": title,
+                "message": message
+            });
+            resultsToast.fire();
+        } else {
+            console.log('Not in Salesforce APP so no toast available, showing alert instead')
+            alert(`${title}: ${message}`)
+        }
     }
 })
