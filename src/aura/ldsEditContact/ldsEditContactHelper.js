a1c3d4479e00c3145bd2ca18d213afe1ef51a1fa
Miguel Núñez Díaz-Montes <m.nunez.diaz-montes@accenture.com>
2018-10-13 12:45:11 +0200

    Implement Contact Show | Edit | App ; Add show toast fix

---
+++ src/aura/ldsEditContact/ldsEditContactHelper.js
@@ -0,0 +1,15 @@
+({
+    showToast: function (title, message) {
+        var resultsToast = $A.get("e.force:showToast");
+        if (resultsToast) {
+            resultsToast.setParams({
+                "title": title,
+                "message": message
+            });
+            resultsToast.fire();
+        } else {
+            console.log('Not in Salesforce APP so no toast available, showing alert instead')
+            alert(`${title}: ${message}`)
+        }
+    }
+})
