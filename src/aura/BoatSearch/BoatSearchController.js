17b1322886daaf52f04bd479f9e5dc1990fea3f8
Miguel Núñez Díaz-Montes <m.nunez.diaz-montes@accenture.com>
2018-11-26 00:25:14 +0100

    Add aura id so component is visible ; Get component in the controller

--- src/aura/BoatSearch/BoatSearchController.js
+++ src/aura/BoatSearch/BoatSearchController.js
@@ -2,5 +2,6 @@
     onFormSubmit: function(component, event, helper) {
         var formData = event.getParam('formData')
         console.log('event controlled', formData.boatTypeId)
+        var searchComponent = component.find("search");
     }
 })
