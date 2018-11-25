2f0562aca9570d9f71a9f16a257ad9f7c5904628
Miguel Núñez Díaz-Montes <m.nunez.diaz-montes@accenture.com>
2018-11-25 13:20:33 +0100

    Refactor into helper functions

--- src/aura/expenses/expensesHelper.js
+++ src/aura/expenses/expensesHelper.js
@@ -1,31 +1,25 @@
 ({
-    createExpense: function(component, expense) {
-        var action = component.get("c.saveExpense");
+    saveExpense: function(component, expense, callback) {
+        var action = component.get("c.saveExpense")
         action.setParams({
             "expense": expense
-        });
-        action.setCallback(this, function(response){
-            var state = response.getState();
+        })
+        if (callback) {
+            action.setCallback(this, callback)
+        }
+        $A.enqueueAction(action)
+    },
+    createExpense: function(component, expense) {
+        this.saveExpense(component, expense, function(response) {
+            var state = response.getState()
             if (state === "SUCCESS") {
-                var expenses = component.get("v.expenses");
-                expenses.push(response.getReturnValue());
-                component.set("v.expenses", expenses);
+                var expenses = component.get("v.expenses")
+                expenses.push(response.getReturnValue())
+                component.set("v.expenses", expenses)
             }
-        });
-        $A.enqueueAction(action);
+        })
     },
     updateExpense: function(component, expense) {
-        var action = component.get("c.saveExpense");
-        action.setParams({
-            "expense": expense
-        });
-        action.setCallback(this, function(response){
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                // do nothing!
-            }
-        });
-        $A.enqueueAction(action);
-    },
-
+        this.saveExpense(component, expense)
+    }
 })
