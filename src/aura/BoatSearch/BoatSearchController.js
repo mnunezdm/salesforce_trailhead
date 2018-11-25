0f90316beb62e9c16c34e9bcdf515d6ad0ffc886
Miguel Núñez Díaz-Montes <m.nunez.diaz-montes@accenture.com>
2018-11-26 00:25:38 +0100

    Declare public search method

--- src/aura/BoatSearch/BoatSearchController.js
+++ src/aura/BoatSearch/BoatSearchController.js
@@ -3,5 +3,7 @@
         var formData = event.getParam('formData')
         console.log('event controlled', formData.boatTypeId)
         var searchComponent = component.find("search");
+        searchComponent.search(formData.boatTypeId)
+        console.log('searchComponent called')
     }
 })
