654f5eb2dfa4b17390666eccd6dda9216568d4c2
Miguel Núñez Díaz-Montes <m.nunez.diaz-montes@accenture.com>
2018-11-25 13:18:06 +0100

    Implement server side update

--- src/aura/expenses/expensesHelper.js
+++ src/aura/expenses/expensesHelper.js
@@ -13,5 +13,19 @@
             }
         });
         $A.enqueueAction(action);
-    }
+    },
+    updateExpense: function(component, expense) {
+        var action = component.get("c.saveExpense");
+        action.setParams({
+            "expense": expense
+        });
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                // do nothing!
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
 })
