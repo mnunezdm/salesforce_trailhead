beecf681e9e4784daa6cac11205d68827256448e
Miguel Núñez Díaz-Montes <m.nunez.diaz-montes@accenture.com>
2018-11-25 19:39:24 +0100

    Implement Boat Search Form ; Add server side controller to fetch BoatTypes

--- src/aura/BoatSearchForm/BoatSearchFormHelper.js
+++ src/aura/BoatSearchForm/BoatSearchFormHelper.js
@@ -1,2 +1,16 @@
 ({
+    fetchBoatTypes : function(component) {
+        var action = component.get('c.getBoatTypes')
+        action.setCallback(this, function(response) {
+            console.log(response.getState())
+            if (response.getState() === 'SUCCESS') {
+                var boatTypes = response.getReturnValue()
+                console.log(`SUCCESS fetching boat types ${boatTypes.length}`)
+                component.set('v.boatTypes', boatTypes)
+            } else {
+                console.log('ERROR fetching boat types')
+            }
+        })
+        $A.enqueueAction(action)
+    }
 })
