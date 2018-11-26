0ef6b8a67c7af44cee596776d745f6cbf8bf4f33
Miguel Núñez Díaz-Montes <m.nunez.diaz-montes@accenture.com>
2018-11-26 01:21:31 +0100

    Add boat selected event ; Fire it on on Tile Click ; Add selected flag

--- src/aura/BoatTile/BoatTileController.js
+++ src/aura/BoatTile/BoatTileController.js
@@ -1,2 +1,8 @@
 ({
+    onBoatClick: function(component, event, helper) {
+        var event = component.getEvent('boatSelect')
+        var boatId = component.get('v.boat').Id
+        event.setParams({boatId})
+        event.fire()
+    }
 })
