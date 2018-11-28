2d09ab00ad38c94925d7bd53efd89477b576a169
Miguel Núñez Díaz-Montes <m.nunez.diaz-montes@accenture.com>
2018-11-29 00:26:03 +0100

    Add force record data ; Add handlers for events ; Add missing component meta

---
+++ src/aura/AddBoatReview/AddBoatReviewController.js
@@ -0,0 +1,13 @@
+({
+    doInit : function(component, event, helper) {
+        helper.onInit(component, event, helper)
+    },
+    onSave : function(component, event, helper) {
+        helper.showMessage("Success", "The record has been saved successfully.")
+        helper.onInit(component, event, helper)
+        helper.notifyReviewAdded(component)
+    },
+    onRecordUpdated : function(component, event, helper) {
+        helper.showMessage("Success", "The record has been updated successfully.")
+    }
+})
---
+++ src/aura/AddBoatReview/AddBoatReviewHelper.js
@@ -0,0 +1,38 @@
+({
+    onInit : function(component, event, helper) {
+        var lightningDataService = component.find('service')
+        lightningDataService.getNewRecord('BoatReview__c', null, false, () => {
+            console.log('AddBoatReviewHelper-INFO: Template Initialized')
+            helper.setRecordId(component, helper)
+            helper.checkErrors(component, helper)
+        })
+    },
+    setRecordId : function(component, helper) {
+        var record = component.get('v.boatReview')
+        var boat = component.get('boat')
+        console.log(boar)
+        record.Boat__c = boat.Id
+        component.set('v.record', record)
+    },
+    checkErrors : function(component, helper) {
+        var errors = component.get('v.errors')
+        if (errors) {
+            console.log('AddBoatReviewHelper-ERROR:', errors)
+        }
+    },
+    showMessage : function(title, message){
+        var toast = $A.get("e.force:showToast")
+        if (toast) {
+            toast.setParams({title, message});
+            toast.fire();
+        } else {
+            console.log('AddBoatReviewController-INFO: toast not supported using alert')
+            alert(`${title}: ${message}`)
+        }
+    },
+    notifyReviewAdded : function(component){
+        console.log('AddBoatReviewHelper-INFO: Firing event')
+        var event = component.getEvent('reviewAdded')
+        event.fire()
+    }
+})
