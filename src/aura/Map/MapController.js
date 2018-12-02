d1a0337cede898742cad26acfcf74960e0f34283
Miguel Núñez Díaz-Montes <m.nunez.diaz-montes@accenture.com>
2018-12-02 17:53:12 +0100

    Convert Plot Attributes from int to double ; Set location object ; Wrap Map with card

--- src/aura/Map/MapController.js
+++ src/aura/Map/MapController.js
@@ -3,9 +3,9 @@
         component.set("v.jsLoaded", true);
     },
     onPlotMapMarker : function(component, event, helper) {
-        console.log('Event handled')
         var eventParams = event.getParams()
-        console.log(JSON.parse(JSON.stringify(eventParams)))
-
+        var long = eventParams.long
+        var lat = eventParams.lat
+        component.set('v.location', {long, lat})
     }
 })
