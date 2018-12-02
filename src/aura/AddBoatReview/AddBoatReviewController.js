100205dc206b11a8c229738d546607e4d8b3e6c3
Miguel Núñez Díaz-Montes <m.nunez.diaz-montes@accenture.com>
2018-12-02 12:36:53 +0100

    💄

--- src/aura/AddBoatReview/AddBoatReviewController.js
+++ src/aura/AddBoatReview/AddBoatReviewController.js
@@ -3,9 +3,11 @@
         helper.onInit(component, event, helper)
     },
     onSave : function(component, event, helper) {
-        helper.showMessage("Success", "The record has been saved successfully.")
-        helper.onInit(component, event, helper)
-        helper.notifyReviewAdded(component)
+        component.find("service").saveRecord(saveResult => {
+            helper.showMessage("Success", "The record has been saved successfully.")
+            helper.onInit(component, event, helper)
+            helper.notifyReviewAdded(component)
+        })
     },
     onRecordUpdated : function(component, event, helper) {
         helper.showMessage("Success", "The record has been updated successfully.")
