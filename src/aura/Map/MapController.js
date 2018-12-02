0651eb87cadefadc9983e2cbf2c0a8914f1e2a34
Miguel Núñez Díaz-Montes <m.nunez.diaz-montes@accenture.com>
2018-12-02 17:46:20 +0100

    Create Map Marker event ; Register it ; Fire it ; Handle it

--- src/aura/Map/MapController.js
+++ src/aura/Map/MapController.js
@@ -1,5 +1,11 @@
 ({
     jsLoaded: function(component) {
         component.set("v.jsLoaded", true);
-    }  
+    },
+    onPlotMapMarker : function(component, event, helper) {
+        console.log('Event handled')
+        var eventParams = event.getParams()
+        console.log(JSON.parse(JSON.stringify(eventParams)))
+
+    }
 })
