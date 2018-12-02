5f0c06de74491f3defb38f2085e185cc565b59b8
Miguel Núñez Díaz-Montes <m.nunez.diaz-montes@accenture.com>
2018-12-02 13:57:34 +0100

    Retrieve boat reviews from apex controller

--- src/aura/BoatReviews/BoatReviewsHelper.js
+++ src/aura/BoatReviews/BoatReviewsHelper.js
@@ -1,5 +1,18 @@
 ({
-    helperMethod : function() {
+    onInit : function(component, helper) {
+        var boatId = component.get('v.boat').Id
+        console.log(`BoatReviewsHelper-INFO: Preparing to get reviews for '${boatId}'`)
 
+        var action = component.get('c.getAll')
+        action.setParams({boatId})
+        action.setCallback(this, response => {
+            if (response.getState() === 'SUCCESS') {
+                console.log('BoatReviewsHelper-INFO: Getting reviews was successfull')
+                component.set('v.boatReviews', response.getReturnValue())
+            } else {
+                console.log('BoatReviewsHelper-INFO: Getting reviews failed')
+            }
+        })
+        $A.enqueueAction(action)
     }
 })
