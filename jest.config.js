3e5931bc15e24e93747f12390fb10157a40ccb0e
Miguel Núñez Díaz-Montes <miguel.nunez.diaz-montes.contractor@bbva.com>
2020-09-24 20:10:48 +0200

    Add stub implementation of the generic lightning button

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 module.exports = {
     ...jestConfig,
     // add any custom configurations here
+    moduleNameMapper: {
+        '^lightning/button$': './test/jest-mocks/lightning/button'
+    }
 };
