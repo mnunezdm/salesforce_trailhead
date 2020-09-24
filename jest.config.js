f9d6b3c3619121235a7ec392483162ceeebebb98
Miguel Núñez Díaz-Montes <miguel.nunez.diaz-montes.contractor@bbva.com>
2020-09-24 20:21:15 +0200

    Add mocked component from managed package (cof cof) ; Fix import of mocked components

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     ...jestConfig,
     // add any custom configurations here
     moduleNameMapper: {
-        '^lightning/button$': './test/jest-mocks/lightning/button'
+        '^lightning/button$': '<rootDir>/main/test/jest-mocks/lightning/button',
+        '^thunder/hammerButton$': '<rootDir>/main/test/jest-mocks/thunder/hammerButton',
     }
 };
