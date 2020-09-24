fd474d7d0736809b6697f4062d622cb9589b61ff
Miguel Núñez Díaz-Montes <miguel.nunez.diaz-montes.contractor@bbva.com>
2020-09-24 19:11:37 +0200

    Add test for testing the change of the property changes the template

--- main/default/lwc/unitTest/__tests__/unitTest.test.js
+++ main/default/lwc/unitTest/__tests__/unitTest.test.js
@@ -20,4 +20,23 @@ describe('c-unit-test', () => {
     const div = element.shadowRoot.querySelector('div');
     expect(div.textContent).toBe('Unit 5 alive!');
   });
+
+  it('displays unit status with updated unitNumber', () => {
+    const element = createElement('c-unit-test', {
+     is: UnitTest
+    });
+    // Add the element to the jsdom instance
+    document.body.appendChild(element);
+    // Update unitNumber after element is appended
+    element.unitNumber = 6
+    const div = element.shadowRoot.querySelector('div');
+    // Verify displayed unit status
+    expect(div.textContent).not.toBe('Unit 6 alive!');
+    // Return a promise to wait for any asynchronous DOM updates. Jest
+    // will automatically wait for the Promise chain to complete before
+    // ending the test and fail the test if the promise rejects.
+    return Promise.resolve().then(() => {
+      expect(div.textContent).toBe('Unit 6 alive!');
+    });
+  });
 });
