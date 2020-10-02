556fda7d02f0eb24dee9bdedad911e1be6dc703e
Miguel Núñez Díaz-Montes <miguel.nunez.diaz-montes.contractor@bbva.com>
2020-10-02 23:31:12 +0200

    feat(step 7): Implement boats near me component

---
+++ force-app/main/default/lwc/boatsNearMe/boatsNearMe.js
@@ -0,0 +1,77 @@
+import { LightningElement, wire, api, track } from 'lwc';
+
+import getBoatsByLocation from '@salesforce/apex/BoatDataService.getBoatsByLocation';
+
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+// imports
+const LABEL_YOU_ARE_HERE = 'You are here!';
+const ICON_STANDARD_USER = 'standard:user';
+const ERROR_TITLE = 'Error loading Boats Near Me';
+const ERROR_VARIANT = 'error';
+export default class BoatsNearMe extends LightningElement {
+  @api boatTypeId = '';
+  @track mapMarkers = [];
+  isLoading = true;
+  isRendered;
+  latitude;
+  longitude;
+
+  // Add the wired method from the Apex Class
+  // Name it getBoatsByLocation, and use latitude, longitude and boatTypeId
+  // Handle the result and calls createMapMarkers
+  @wire(getBoatsByLocation, { latitude: '$latitude', longitude: '$longitude', boatTypeId: '$boatTypeId' })
+  wiredBoatsJSON({ error, data }) {
+    if (data) {
+      this.error = undefined;
+      this.isLoading = false;
+      this.createMapMarkers(JSON.parse(data));
+    } else if (error) {
+      const evt = new ShowToastEvent({
+        title: ERROR_TITLE,
+        message: error,
+        variant: ERROR_VARIANT,
+      });
+      this.dispatchEvent(evt);
+    }
+  }
+
+  // Controls the isRendered property
+  // Calls getLocationFromBrowser()
+  renderedCallback() {
+    if (!this.isRendered) {
+      this.getLocationFromBrowser();
+    }
+    this.isRendered = true;
+  }
+
+  // Gets the location from the Browser
+  // position => {latitude and longitude}
+  getLocationFromBrowser() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.longitude = position.coords.longitude;
+      this.latitude = position.coords.latitude;
+    });
+  }
+
+  // Creates the map markers
+  createMapMarkers(boatData) {
+    const newMarkers = boatData.map(boat => ({
+      location: {
+        Latitude: boat.Geolocation__Latitude__s,
+        Longitude: boat.Geolocation__Longitude__s,
+      },
+      title: boat.Name,
+    }));
+    newMarkers.unshift({
+      location: {
+        Latitude: this.latitude,
+        Longitude: this.longitude,
+      },
+      title: LABEL_YOU_ARE_HERE,
+      icon: ICON_STANDARD_USER,
+    });
+    this.mapMarkers = newMarkers;
+    this.isLoading = false;
+  }
+}
